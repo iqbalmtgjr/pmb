151a9637d474922a357cf44d2d2418f590d0a044
Muhammad Iqbal <iqbalmtgjr@gmail.com>
2023-11-16 09:41:53 +0700

    aksdasd

---
+++ resources/views/info/index.blade.php
@@ -0,0 +1,110 @@
+@extends('layouts.master')
+@section('content')
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Info Pembayaran</h6>
+    </div>
+    <div class="card-body">
+        <div class="form-group row">
+       <label for="inputEmail3" class="col-sm-4 col-form-label">Nomor Pendaftaran</label>
+       <div class="col-sm-8">
+         <span class="form-control">19656</span>
+       </div>
+     </div>
+       <div class="form-group row">
+       <label for="inputEmail3" class="col-sm-4 col-form-label">Jalur Pendaftaran</label>
+       <div class="col-sm-8">
+                                 <span class="form-control">Prestasi</span>
+       </div>
+     </div>
+     <div class="form-group row">
+       <label for="inputEmail3" class="col-sm-4 col-form-label">Nama</label>
+       <div class="col-sm-8">
+         <span class="form-control">Muhammad Iqbal</span>
+       </div>
+     </div>
+     <div class="form-group row">
+       <label for="inputPassword3" class="col-sm-4 col-form-label">NISN</label>
+       <div class="col-sm-8">
+           <span class="form-control">123456789</span>
+       </div>
+     </div>
+      <div class="form-group row">
+       <label for="inputEmail3" class="col-sm-4 col-form-label">Program Studi Pilihan I</label>
+       <div class="col-sm-8">
+           <span class="form-control">Pendidikan Komputer</span>
+       </div>
+     </div>
+     <div class="form-group row">
+       <label for="inputEmail3" class="col-sm-4 col-form-label">Program Studi Pilihan II</label>
+       <div class="col-sm-8">
+           <span class="form-control">Pendidikan Bahasa dan Sastra Indonesia</span>
+       </div>
+     </div>
+     
+<!-- Belum ada keputusan tes Awal -->
+
+<div class="card-body bg-light">
+          <a href="https://daftar.persadakhatulistiwa.ac.id/pmb/pdflulusa5" class="btn btn-warning" target="_blank">Download Status Penerimaan MABA</a>
+       <div class="form-group row">
+       <label for="inputEmail3" class="col-sm-4 col-form-label">Status Penerimaan</label>
+       <div class="col-sm-8">
+           <span class="form-control">LULUS</span>
+       </div>
+       </div>
+                               <div class="form-group row">
+           <label for="inputEmail3" class="col-sm-4 col-form-label">Diterima pada Prodi</label>
+           <div class="col-sm-8">
+               <span class="form-control">Pendidikan Komputer</span>
+           </div>
+           </div>
+                           <div class="form-group row">
+       <label for="inputEmail3" class="col-sm-4 col-form-label">Catatan Penerimaan</label>
+       <div class="col-sm-8">
+           <textarea class="form-control" disabled="">Selamat atas kelulusan Anda. Mohon Lengkapi data pribadi di akun PMB dan selanjutnya silahkan melakukan pembayaran registrasi. Terima kasih.</textarea>
+       </div>
+       </div>
+       <table class="table table-bordered">
+     <thead>                  
+       <tr>
+         <th style="width: 10px">#</th>
+         <th>Item Pembayaran</th>
+         <th style="width: 40px">Biaya(Rp.)</th>
+       </tr>
+     </thead>
+     <tbody>
+       <tr>
+         <td>1.</td>
+         <td>Biaya Registrasi Mahasiswa Baru</td>
+         <td>1.510.000</td>
+       </tr>
+       <tr>
+         <td>2.</td>
+         <td>Biaya Pengembangan Kampus</td>
+         <td>3.250.000</td>
+       </tr>
+       <tr>
+         <td>3.</td>
+         <td>SPP Tetap / Semester</td>
+         <!--<td>2.150.000</td>-->
+         <td>2.280.000</td>
+       </tr>
+       
+     </tbody>
+   </table>
+   <!--<p>Pembayaran biaya Registrasi Mahasiswa Baru, Pengembangan kampus, SPP Tetap/Semester dilakukan pada tanggal <b></b></p>-->
+   <p>Pembayaran biaya Registrasi Mahasiswa Baru, Pengembangan kampus, SPP Tetap/Semester dilakukan mulai dari dinyatakan lulus sampai dengan 31 Agustus 2023</p>
+   <div class="alert-warning p-2 text-center"><h5><i class="icon fas fa-ban"></i> Perhatian!!!</h5><strong>Pembayaran hanya melalui Teller Bank KALBAR</strong></div>
+   <p>Silahkan Melakukan Pembayaran Sebesar <b> Rp. 7.040.000</b>, melalui Teller Bank KALBAR ke Nomor Rekening <b>4010006517 (BANK KALBAR) </b> Atas Nama PRKMPULN BDN PEND KARYA BANGSA. <i>Masukan Berita pada keterangan transfer bank anda sesuai dengan nomor pendaftaran yaitu : </i><b>19656</b>. Setelah Melakukan Pembayaran Upload Bukti Transaksi (Slip Bank / Struk ATM) ke dalam Akun PMB ini dan melakukan Konfirmasi via WA/SMS ke Nomor 082155964080, konfirmasi pembayaran paling lama dilakukan 2 x 24 jam setelah pembayaran.</p> 
+   <h2 class="text-danger">Mahasiswa Baru wajib mengikuti kegiatan PKKMB pada tanggal 28 Agustus - 2 September 2023</h2>
+   
+   <p><a href="https://daftar.persadakhatulistiwa.ac.id/pmb/konfirbayar" class="btn btn-primary">Konfirmasi Pembayaran</a></p>
+        </div><br>
+        <div class="form-group row">
+      <a href="https://daftar.persadakhatulistiwa.ac.id/pmb/formcalon" style="margin-left: 7px;">Lanjutkan untuk melengkapi data anda</a>
+     </div>
+
+        
+   </div>
+</div>
+@endsection
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,80 @@
+
+ <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">PMB Persada</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    {{-- <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li> --}}
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Pendaftaran
+    </div>
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{url('info')}}">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>Info Pembayaran</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Form Calon Mahasiswa</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Form Pendidikan Terakhir</span></a>
+    </li>
+    
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Upload Nilai Semester</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Program Studi</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Form Orang Tua</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Form Upload Berkas</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
